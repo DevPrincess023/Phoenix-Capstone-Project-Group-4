53715842c56367656c8fa8b064888f6fccade6c6
Mercy Ezeobi <merciliopie016@gmail.com>
2026-06-12 16:09:44 -0700

    Refactor App component to include new sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import DataTable from "./components/DataTable";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Video from "./IVideo";
+import PlanetImage from "./PlanetImage";
+import DataTable from "./DataTable";
+import PlanetaryContactForm from "./PlanetaryContactForm";
+import SuccessModal from "./SuccessModal";
+import Footer from "./Footer";
 
 function App() {
    return (
     <div>
-      <DataTable/>
+       <Hero/>
+       <Navbar/>
+       <Video/>
+       <PlanetImage/>
+       <DataTable/>
+       <PlanetaryContactForm/>
+       <SuccessModal/>
+       <Footer/>
     </div>
  );
 }
+
+export default App;
 
 export default App;
