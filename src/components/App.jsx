0ce6ebdd02b2e8e78026973f47739705d302647c
Mercy Ezeobi <merciliopie016@gmail.com>
2026-06-12 15:42:10 -0700

    Add imports for Hero, Navbar, Video, and PlanetImage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,10 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Video from "./component/Video";
+import PlanetImage from "./component/PlanetImage";
 import DataTable from "./components/DataTable";
+import 
+
 
 function App() {
    return (
