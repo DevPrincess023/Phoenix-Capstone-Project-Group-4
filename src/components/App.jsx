e17b2a6a2b02b3559cbe28bcb075cf5aa1146351
Mercy Ezeobi <merciliopie016@gmail.com>
2026-06-12 18:19:32 -0700

    Fix import paths for components in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Video from "./component/Video";
-import PlanetImage from "./component/PlanetImage";
+import Video from "./components/Video";
+import PlanetImage from "./components/PlanetImage";
 import DataTable from "./components/DataTable";
 import PlanetaryContactForm from "./components/PlanetaryContactForm";
 import SuccessModal from "./components/SuccessModal";
-import Footer from "./component/Footer";
+import Footer from "./components/Footer";
 
 function App() {
    return (
     <>
-       <Hero/>
        <Navbar/>
+       <Hero/>
        <Video/>
        <PlanetaryImage/>
        <DataTable/>
